173af6e712a30d775ef3e43be447431a9ba27cd3
thtsuman <tht.suman@gmail.com>
2020-12-20 13:02:48 +0530

    adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ITodo {
+	id: number
+	content: string
+	isCompleted: boolean
 }
 
-export default App;
+type FormElement = React.FormEvent<HTMLFormElement>
+
+const App = (): JSX.Element => {
+	const [value, setValue] = useState<string>("")
+	const [todos, setTodos] = useState<ITodo[]>([])
+	const [peoples, setPeoples] = useState([])
+
+	useEffect(() => {
+		getResp()
+	}, [])
+
+	const getResp = async () => {
+		const response = await fetch("https://api.pokemontcg.io/v1/cards")
+			.then((resp) => resp.json())
+			.then((resp) => resp.results)
+
+		setPeoples(response)
+	}
+
+	// getResp()
+
+	const handleAddTodo = (e: FormElement): void => {
+		e.preventDefault()
+		const newTodo: ITodo = {
+			content: value,
+			id: todos.length,
+			isCompleted: false,
+		}
+		setTodos((todos) => [...todos, newTodo])
+		setValue("")
+	}
+
+	const handleCheckTodo = (item: ITodo): void => {
+		setTodos(todos.map((todo: ITodo) => (todo.id === item.id ? { ...todo, isCompleted: !todo.isCompleted } : todo)))
+	}
+
+	return (
+		<div>
+			<h1>Todo App</h1>
+			<form onSubmit={handleAddTodo}>
+				<input type="text" required value={value} onChange={(e) => setValue(e.target.value)} />
+				<button type="submit">Add Todo</button>
+			</form>
+			<br />
+			<br />
+			{todos?.map((item, i) => (
+				<div key={i}>
+					{item.content}
+					<input type="checkbox" checked={item.isCompleted} onChange={() => handleCheckTodo(item)} />
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default App
